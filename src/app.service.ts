880f409bff50497fd593e149fcf0b0a5627adba1
pierresouza <pierre.s3@hotmail.com>
2024-07-19 11:11:40 -0300

    create nest project

---
+++ src/app.service.ts
@@ -0,0 +1,11 @@
+import {Injectable} from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+  getTurma(): string {
+    return '<i>Turma Javascript 05</i>';
+  }
+}
